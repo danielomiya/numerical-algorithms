23bc18415fafb0e45c1fe6469f47667043f80e3f
Daniel Omiya <danielomiya@gmail.com>
2020-12-29 21:37:32 -0300

    Add Gaussian algorithm and editorconfig

---
+++ gaussian_elimination/__init__.py
@@ -0,0 +1,68 @@
+from typing import List
+
+
+def _validate_system_of_equations(matrix: List[List[float]]) -> None:
+    equations_len = len(matrix)
+    line_len = equations_len + 1
+
+    if not (equations_len >= 1
+            and all(len(s) == line_len for s in matrix)):
+        raise ArithmeticError('malformed system of equations')
+
+
+def gaussian_elimination(matrix: List[List[float]]) -> List[float]:
+    """Given an system of equations,
+    calculates the unknowns using Gaussian method
+
+    Args:
+        matrix (List[List[float]]): system of equations
+
+    Raises:
+        ArithmeticError: thrown when the input is not valid
+
+    Returns:
+        List[float]: values of the unknowns
+    """
+    _validate_system_of_equations(matrix)
+
+    number_equations = len(matrix)
+    unknowns = [None] * number_equations
+
+    # elimination process
+    for i in range(number_equations-1):  # iter rows
+        # search for pivot index in column
+        pivot = min(
+            (p for p, row in enumerate(matrix) if row[i] != 0 and i <= p < number_equations),
+            default=None)
+
+        if pivot is None:
+            raise ArithmeticError('no unique solution exists')
+
+        if pivot != i:
+            # if pivot is not current row, swipe rows
+            matrix[pivot], matrix[i] = matrix[i], matrix[pivot]
+
+        for j in range(i+1, number_equations):
+            m = matrix[j][i]/matrix[i][i]
+            subtrahend = [a*m for a in matrix[i]]
+
+            # Ej = Ej - m * Ei
+            matrix[j] = [m-s for m, s in zip(matrix[j], subtrahend)]
+
+    if matrix[number_equations-1][number_equations-1] == 0:
+        raise ArithmeticError('no unique solution exists')
+
+    # backward substitution
+    for i in reversed(range(number_equations)):
+        summ = 0
+
+        for j in range(i+1, number_equations):
+            summ += matrix[i][j] * unknowns[j]
+
+        right = matrix[i][number_equations]  # right side of equation
+        numerator = right - summ
+        denominator = matrix[i][i]
+
+        unknowns[i] = numerator / denominator
+
+    return unknowns
---
+++ tests/gaussian_elimination_test.py
@@ -0,0 +1,45 @@
+import pytest
+from gaussian_elimination import gaussian_elimination
+
+@pytest.mark.parametrize('matrix,expected', [
+    ([  # example
+        [1,  1,  0,   3,   4],
+        [0, -1, -1,  -5,  -7],
+        [0,  0,  3,  13,  13],
+        [0,  0,  0, -13, -13],
+    ], [-1, 2, 0, 1]),
+    ([  # 3.a
+        [1, -1, 3,  2],
+        [3, -3, 1, -1],
+        [1,  1, 0,  3],
+    ], [1.1875, 1.8125, 0.875]),
+    ([  # 3.b
+        [ 2, -1.5, 3, 1],
+        [-1,    0, 2, 3],
+        [ 4, -4.5, 5, 1],
+    ], [-1, 0, 1]),
+    ([  # 3.c
+        [2,   0,   0, 0,    3],
+        [1, 1.5,   0, 0,  4.5],
+        [0,  -3, 0.5, 0, -6.6],
+        [2,  -2,   1, 1,  0.8],
+    ], [1.5, 2, -1.2, 3]),
+])
+def test_main(matrix, expected):
+    result = gaussian_elimination(matrix)
+
+    assert pytest.approx(expected) == result
+
+
+def test_error():
+    matrix = [  # 3.d
+        [1,  1,  0, 1,  2],
+        [2,  1, -1, 1,  1],
+        [4, -1, -2, 2,  0],
+        [3, -1, -1, 2, -3],
+    ]
+
+    with pytest.raises(ArithmeticError) as exc_info:
+        gaussian_elimination(matrix)
+
+    assert 'no unique solution exists' == str(exc_info.value)
