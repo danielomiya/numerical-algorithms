83567e02bc567a93120e39cd69edf41dfafcdbc6
Daniel Omiya <danielomiya@gmail.com>
2021-01-23 02:16:44 -0300

    Add support to Python typing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
